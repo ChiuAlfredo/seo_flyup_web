65e954a41801694b10e4b6a5d9cfa70cfc32ba2e
alfredochiu <petercy32@gmail.com>
2025-02-04 12:17:19 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
   base: '/seo_flyup_web/',
   build: {
     outDir: 'dist',  // 指定輸出目錄
-    emptyOutDir: true  // 構建前清空輸出目錄
+    emptyOutDir: true,  // 構建前清空輸出目錄
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   }
 })
