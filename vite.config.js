853df782be41b50da91d3f29870a23b184889961
alfredochiu <petercy32@gmail.com>
2025-02-04 12:39:16 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: '/seo_flyup_web/',  // Add public path for GitHub Pages
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/seo_flyup_web/'
+    : '/',
   build: {
     outDir: 'dist',  // 指定輸出目錄
     emptyOutDir: true,  // 構建前清空輸出目錄
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
-  }
+  },
 })
