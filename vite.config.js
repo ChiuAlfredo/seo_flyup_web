172a46f72e5f65f7494efe1328dc98eba14705fe
alfredochiu <petercy32@gmail.com>
2025-02-04 12:44:18 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === 'production'
+  base: process.env.NODE_ENV === 'production'
     ? '/seo_flyup_web/'
     : '/',
   build: {
