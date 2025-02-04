5ac75503063b6bd1cbd8751e22fe9698436d6f93
alfredochiu <petercy32@gmail.com>
2025-02-04 12:25:17 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/seo_flyup_web/',
+  publicPath: '/seo_flyup_web/',  // Add public path for GitHub Pages
   build: {
     outDir: 'dist',  // 指定輸出目錄
     emptyOutDir: true,  // 構建前清空輸出目錄
