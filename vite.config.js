f2e3906e8dd79ca34ba3730964dd41ff21e90298
alfredochiu <petercy32@gmail.com>
2025-02-12 12:12:39 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' && process.env.DEPLOY_TARGET !== 'custom_domain'
-    ? '/seo_flyup_web/'
-    : '/',
+  base: '/',
   build: {
     outDir: 'dist',  // 指定輸出目錄
     emptyOutDir: true,  // 構建前清空輸出目錄
